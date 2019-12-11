21a0cc34b7600bf046df9af4306cb3a6f986312e
Ashish <yesashishs@gmail.com>
2019-12-11 11:46:59 -0500

    Add ViewController Class

---
+++ whatsInMyJunk/whatsInMyJunk/WhatYouAteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WhatYouAteViewController.swift
+//  whatsInMyJunk
+//
+//  Created by Joseph on 11/12/19.
+//  Copyright © 2019 Zain Lakhani. All rights reserved.
+//
+
+import UIKit
+
+class WhatYouAteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
